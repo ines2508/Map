d30f60151ca0e576020099f3f21a531c09c1a017
Ewelina-Pluta <ines2508@yahoo.com>
2019-08-19 11:20:01 +0200

    Added flexbox style's

--- neighborhood-map/src/App.js
+++ neighborhood-map/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
+
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import Map from './components/Map'
@@ -8,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <Header></Header>
-      <div>
+      <div className="main">
         <Sidebar></Sidebar>
         <Map></Map>
       </div>
