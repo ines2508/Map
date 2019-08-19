89d81a80c6df1ac6b35c3ac18a4d094f6f526588
Ewelina-Pluta <ines2508@yahoo.com>
2019-08-19 13:18:49 +0200

    Added FourSquare API

--- neighborhood-map/src/App.js
+++ neighborhood-map/src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
+import axios from 'axios'
 
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
@@ -8,13 +9,35 @@ import Map from './components/Map'
 class App extends Component {
   
   componentDidMount() {
+    this.getVenues()
     this.renderMap()
   }
 
   // followed by Yahya Elharony
   // https://www.youtube.com/watch?v=W5LhLZqj76s
   
-  // setting up Google Map
+  // setting up FourSquare API
+  getVenues = () => {
+    const endPoint = 'https://api.foursquare.com/v2/venues/explore?'
+    const parameters = {
+      client_id:  'DWLFRW5WP4RGSFSPTQEBRZ3QGBQP1EYNWL45NMQNTWXJ4K3J',
+      client_secret: 'A0SSRHQR2MSPEFLXJ3I3OSJH23XDYL13XEASLLVV0JMLYYNK',
+      query: 'arts',
+      // Barcelona
+      ll: '41.390205, 2.154007',
+      v: '20180323'
+    }
+
+  // Promise
+
+  axios.get(endPoint + new URLSearchParams(parameters))
+  .then(response => {console.log(response.data.response.groups[0].items)})
+  .catch(error => {console.log('There are no data to display' + error)})
+  }
+
+ 
+
+  // setting up Google Map API
   renderMap = () => {
     loadScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyBZaSwddTJZ6RrHZ7QTcFtEgvScuOmZ_uk&callback=initMap')
     window.initMap = this.initMap
@@ -26,6 +49,8 @@ class App extends Component {
           zoom: 8
     });
   }
+
+  
 
   render() {  
     return (
